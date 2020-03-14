dddbe50d664e20ba5cfd85718b77591d498bfb13
James Benjamin <james.abenj@gmail.com>
2020-03-14 23:52:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,11 @@ module FormsLab
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[pirate[:name]], params[pirate[:weight], params[pirate][:height]])
+      @pirate = Pirate.new(params[:pirate][:name]], params[pirate[:weight], params[pirate][:height]])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end 
+      @ships = Ship.all
       erb :show
     end
 
