e9248acf1e1380fc8a3b8611faa68fee6bd2bdcc
James Benjamin <james.abenj@gmail.com>
2020-03-14 23:29:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,24 @@
 require './environment'
+require_relative 'app/models/pirate.rb'
+require_relative 'app/models/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/pirates' do
+      erb :new
+    end
+
+    post '/pirates' do
+      erb :show
+    end
+
+
 
   end
 end
